cbce07b1e3c6840705045c6ec0ec1d769c1f48ce
JeniaD <53406085+JeniaD@users.noreply.github.com>
2020-05-29 21:24:58 +0300

    First version of assistant.
    It may be not exactly first version, because first version was deleted from my profile.

---
+++ Source 1.0.py
@@ -0,0 +1,36 @@
+import wikipedia
+
+def FindAnswer(s):
+    try:
+        return wikipedia.summary(s)
+    except:
+        return "No answer."
+
+
+def FindAnswer(s, c):
+    try:
+        return wikipedia.summary(s, sentences = c)
+    except:
+        return No answer.
+
+def FindPage(s):
+    try:
+        return wikipedia.page(s) #title, url, links[], content;
+    except:
+        return No answer.
+        
+def Search(s):
+    return wikipedia.search(s)
+
+def WriteDefinition(q):
+    try:
+        res = FindPage(q)
+        print(FindAnswer(q, 3))
+        print("\nUrl: ", res.url)
+        print("\nDid you mean: ", res.links, "\b?\n\n")
+    except:
+        print("\nError!\n")
+
+while True:
+    s = input(">")
+    WriteDefinition(s)
